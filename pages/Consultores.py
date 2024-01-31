298e8ecd7ffd3834d8ae31ad6abaac313fe73df5
shady <shadyrajaab@gmail.com>
2024-01-31 11:25:53 -0300

    Alterando nome do arquivo

--- pages/Consultores.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from dataframes.objects import get_consultores
-from plots.scatter import plot_line
-
-st.title('Consultores')
-
-consultor = st.sidebar.selectbox('Selecionar Consultor', options = get_consultores())
-
-plot_line(consultor)
