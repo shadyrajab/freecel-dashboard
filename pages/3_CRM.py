e54cb734f5aeba6c56d56ee33c4d69d1870fc12a
shady <shadyrajaab@gmail.com>
2024-02-06 11:15:53 -0300

    formatando os cnpj

--- pages/3_CRM.py
+++ pages/3_CRM.py
@@ -1,6 +1,35 @@
 import streamlit as st
 import pandas as pd 
+import re
+
+def formatar_cnpj(cnpj):
+    cnpj = re.sub(r'\D', '', cnpj)
+    cnpj = cnpj.zfill(14)
+    cnpj_formatado = f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}"
+    
+    return cnpj_formatado
+
 
 df = pd.read_excel('dataframes/excel/crm 05-02.xlsx')
+df.drop(axis = 1, 
+    columns = {
+        '0', 
+        'Pedido Vinculado',
+        'Usuário ADM',
+        'Revisão',
+        'Item',
+        'Data Instalação',
+        'Período',
+        'Cidade Instalação',
+        'Estado Instalação',
+        'Rpon',
+        'Instância',
+        'Consultor na Operadora',
+        'Etapa Item'
+    }, 
+    inplace = True
+)
 
+df = df.astype(str)
+df['Cpf Cnpj'] = df['Cpf Cnpj'].apply(lambda n: formatar_cnpj(n))
 st.dataframe(df)
