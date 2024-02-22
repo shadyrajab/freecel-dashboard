956b0b3711b8e924f4af5fb797bfdb3689cb15a3
shady <shadyrajaab@gmail.com>
2024-02-22 14:26:17 -0300

    feat: adicionar unificação dos rankings

--- dataframe/rankings.py
+++ dataframe/rankings.py
@@ -34,3 +34,53 @@ class Ranking:
         self.ranking_fixa = pd.DataFrame(data['ranking_fixa'])
         self.ranking_avancada = pd.DataFrame(data['ranking_avancada'])
         self.ranking_vvn = pd.DataFrame(data['ranking_vvn'])
+
+    def ranking_geral(self):
+        def renomear_colunas(dataframe, nome_do_ranking):
+            novo_nome_colunas = {
+                'valor_acumulado': f'valor_acumulado_{nome_do_ranking}',
+                'quantidade_de_produtos': f'quantidade_de_produtos_{nome_do_ranking}',
+                'quantidade_de_vendas': f'quantidade_de_vendas_{nome_do_ranking}'
+            }
+            return dataframe.rename(columns=novo_nome_colunas)
+        
+        ranking_consultores = renomear_colunas(self.ranking_consultores, 'consultores')
+        ranking_altas = renomear_colunas(self.ranking_altas, 'altas')
+        ranking_migracao = renomear_colunas(self.ranking_migracao, 'migracao')
+        ranking_fixa = renomear_colunas(self.ranking_fixa, 'fixa')
+        ranking_avancada = renomear_colunas(self.ranking_avancada, 'avancada')
+        ranking_vvn = renomear_colunas(self.ranking_vvn, 'vvn')
+
+        merged_df = pd.merge(ranking_consultores, ranking_altas, on='consultor', how='outer')
+        merged_df = pd.merge(merged_df, ranking_migracao, on='consultor', how='outer')
+        merged_df = pd.merge(merged_df, ranking_fixa, on='consultor', how='outer')
+        merged_df = pd.merge(merged_df, ranking_avancada, on='consultor', how='outer')
+        if len(ranking_vvn.columns):
+            merged_df = pd.merge(merged_df, ranking_vvn, on='consultor', how='outer')
+
+        merged_df.fillna(0, inplace = True)
+
+        merged_df.rename(
+            columns = {
+                'quantidade_de_produtos_consultores': 'Volume',
+                'valor_acumulado_consultores': 'Receita',
+                'quantidade_de_vendas_consultores': 'Clientes',
+                'quantidade_de_produtos_altas': 'Volume',
+                'valor_acumulado_altas': 'Receita',
+                'quantidade_de_produtos_migracao': 'Volume',
+                'valor_acumulado_migracao': 'Receita',
+                'quantidade_de_produtos_fixa': 'Volume',
+                'valor_acumulado_fixa': 'Receita',
+                'quantidade_de_produtos_avancada': 'Volume',
+                'valor_acumulado_avancada': 'Receita',
+
+
+
+            }, inplace = True
+        )  
+
+        merged_df.drop(['quantidade_de_vendas_avancada', 'quantidade_de_vendas_fixa','quantidade_de_vendas_migracao','quantidade_de_vendas_altas'], axis = 1, inplace = True)
+        merged_df.set_index('consultor', inplace = True)
+
+        return merged_df
+
