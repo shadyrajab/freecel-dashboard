842cce93755ffac60c3cd658b599f9c5253f6a18
shady <shadyrajaab@gmail.com>
2024-02-23 10:44:36 -0300

    feat: substituir botões por st.forms. Fixed #16

--- pages/Vendas.py
+++ pages/Vendas.py
@@ -17,7 +17,7 @@ vendas[['matriz', 'porte', 'capital_social', 'situacao_cadastral', 'cep']] = ven
     ['matriz', 'porte', 'capital_social', 'situacao_cadastral', 'cep']
 ].map(remover_ponto)
 
-with open('styles/styles.css', 'r') as styles:
+with open('styles/vendas.css', 'r') as styles:
     css = styles.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
@@ -29,7 +29,6 @@ vendas = vendas[
         'natureza_juridica', 'matriz', 'situacao_cadastral', 'regime_tributario'
     ]
 ] 
-
 
 with st.container(border = True):
     ano = st.selectbox(label = 'Adicionar Filtro:', options = ['Nenhum', 'Ano', 'Mês', 'Tipo'])
@@ -53,41 +52,48 @@ headers = {
 with st.container(border = True):
     with st.expander('Adicionar venda'):
         today = datetime.today().date()
+        
+        with st.form("adicionar_venda"):
+            cnpj = st.text_input('Qual CNPJ do cliente', max_chars = 14)
+            telefone = st.text_input('Qual telefone do cliente')
+            consultor = st.selectbox('Qual o nome do consultor que realizou a venda', options = Stats.consultores())
+            data = st.date_input('Qual a data da venda?', format = 'DD/MM/YYYY', max_value=today)
+            gestor = st.text_input('Qual nome do gestor?')
+            plano = st.text_input('Qual nome do plano vendido')
+            quantidade_de_produtos = st.text_input('Qual a quantidade de produtos vendidos')
+            revenda = st.selectbox('Qual equipe realizou a venda?', options = ['FREECEL', 'VALPARAISO', 'PARCEIRO', 'ESCRITORIO'])
+            tipo = st.selectbox('Qual tipo de venda?', options = ["FIXA", "AVANÇADA", "MIGRAÇÃO PRÉ-PÓS", "VVN","ALTAS"])
+            uf = st.text_input('Qual a uf da venda?', max_chars = 2)
+            valor_do_plano = st.text_input('Qual o valor do plano?')
+            email = st.text_input('Qual o email do cliente?')
 
-        cnpj = st.text_input('Qual CNPJ do cliente', max_chars = 14)
-        telefone = st.text_input('Qual telefone do cliente')
-        consultor = st.selectbox('Qual o nome do consultor que realizou a venda', options = Stats.consultores())
-        data = st.date_input('Qual a data da venda?', format = 'DD/MM/YYYY', max_value=today)
-        gestor = st.text_input('Qual nome do gestor?')
-        plano = st.text_input('Qual nome do plano vendido')
-        quantidade_de_produtos = st.text_input('Qual a quantidade de produtos vendidos')
-        revenda = st.text_input('Qual escritório realizou a venda?')
-        tipo = st.text_input('Qual tipo de venda?')
-        uf = st.text_input('Qual a uf da venda?')
-        valor_do_plano = st.text_input('Qual o valor do plano?')
-        email = st.text_input('Qual o email do cliente?')
+            submit = st.form_submit_button('Enviar')
 
-        if st.button('Adicionar'):
-            params = {
-                "cnpj": cnpj,
-                "telefone": telefone,
-                "consultor": consultor,
-                "data": str(data),
-                "gestor": gestor,
-                "plano": plano,
-                "quantidade_de_produtos": quantidade_de_produtos,
-                "revenda": revenda,
-                "tipo": tipo,
-                "uf": uf,
-                "valor_do_plano": valor_do_plano,
-                "email": email
-            }
-            response = request('PUT', url = url, json = params, headers = headers)
-            st.success('Venda adicionada com sucesso.')
+            if submit:
+                params = {
+                    "cnpj": cnpj,
+                    "telefone": telefone,
+                    "consultor": consultor,
+                    "data": str(datetime.strptime(str(data), '%Y-%m-%d').strftime('%d-%m-%Y')),
+                    "gestor": gestor,
+                    "plano": plano,
+                    "quantidade_de_produtos": quantidade_de_produtos,
+                    "revenda": revenda,
+                    "tipo": tipo,
+                    "uf": uf,
+                    "valor_do_plano": valor_do_plano,
+                    "email": email
+                }
+
+                response = request('PUT', url = url, json = params, headers = headers)
+                st.success('Venda adicionada com sucesso.')
 
     with st.expander('Remover venda'):
-        id_venda = st.text_input('Qual o ID da venda que deseja remover?')
+        with st.form('remover_venda'):
+            id_venda = st.text_input('Qual o ID da venda que deseja remover?')
 
-        if st.button('Remover'):
-            response = request('DELETE', url = url, headers=headers, json = {'id': id_venda})
-            st.success('Venda removida com sucesso.')
+            submit = st.form_submit_button('Remover')
+
+            if submit:
+                response = request('DELETE', url = url, headers=headers, json = {'id': id_venda})
+                st.success('Venda removida com sucesso.')
