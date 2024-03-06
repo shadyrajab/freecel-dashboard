f0b0c0e2830cce779bdd751c22e1f5ed584dabba
shady <shadyrajaab@gmail.com>
2024-03-06 17:46:00 -0300

    feat: adicionar styles

--- pages/Ranking.py
+++ pages/Ranking.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from utils.utils import month_by_numbers, months
 from typing import Optional
 import pandas as pd
+import numpy as np
 
 # Configurando o layout da página
 st.set_page_config(
@@ -57,6 +58,7 @@ st.title(
 
 rankings = load_data(ano, mes)
 df = rankings.full_ranking
+df.fillna(0, inplace=True)
 categories = ['Altas', 'Total', 'VVN', 'Portabilidade', 'Avançada', 'Migração Pré-Pós']
 sub_categories = ['Volume', 'Receita']
 multi_index = pd.MultiIndex.from_product([categories, sub_categories])
@@ -65,17 +67,16 @@ multi_index = pd.MultiIndex.from_product([categories, sub_categories])
 df.set_index('Consultor', inplace=True)
 cell_hover = {  # for row hover use <tr> instead of <td>
     'selector': 'td:hover',
-    'props': [('background-color', '#ffffb3')]
+    'props': [('background-color', 'lightgreen')]
 }
 index_names = {
     'selector': '.index_name',
-    'props': 'font-style: italic; color: darkgrey; font-weight:normal;'
+    'props': 'font-style: italic; color: darkgrey; font-weight:normal;',
 }
 headers = {
     'selector': 'th:not(.index_name)',
     'props': 'background-color: white; color: black;',
     'border': None,
-    'border-radius': '30px'
 }
 
 # Definindo o MultiIndex para o DataFrame
@@ -85,8 +86,25 @@ df = (df.style
     .set_properties(**{'background-color': 'white', 'border': None})
     .set_table_styles([cell_hover, index_names, headers])
     .hide(axis='index')
+    .background_gradient(subset = [
+        ('Altas', 'Volume'),
+        ('Migração Pré-Pós', 'Volume'),
+        ('VVN', 'Volume'),
+        ('Avançada', 'Volume'),
+        ('Portabilidade', 'Volume')
+    ], cmap = 'Reds')
+    .format({
+        ('Altas', 'Receita'): 'R$ {:.1f}',
+        ('Total', 'Receita'): 'R$ {:.1f}',
+        ('Fixa', 'Receita'): 'R$ {:.1f}',
+        ('Portabilidade', 'Receita'): 'R$ {:.1f}',
+        ('Avançada', 'Receita'): 'R$ {:.1f}',
+        ('VVN', 'Receita'): 'R$ {:.1f}',
+        ('Migração Pré-Pós', 'Receita'): 'R$ {:.1f}'
+    })
 )
-html = df.to_html(index=False)
+html = df.to_html(index=False).replace('\n', ' ').replace('<td', '<td style="white-space: nowrap;"')
+
 
 # Exibindo o HTML em Markdown
 st.markdown(f"""
