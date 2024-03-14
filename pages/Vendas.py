643e19a3e6eb60fbc581d1cbbc5df6ecfb564f44
shady <shadyrajaab@gmail.com>
2024-03-14 13:37:01 -0300

    feat: adicionar barra de pesquisa

--- pages/Vendas.py
+++ pages/Vendas.py
@@ -1,35 +1,40 @@
-from utils.utils import (
-    months, 
-    colorir_equipes, 
-    default_index, 
-    formatar_nome,
-    get_form, 
-    colorir_tipo_venda,
-    mask_dataframe,
-    colorir_null_values,
-    colorir_adabas
-)
-from math import ceil
-from dataframe.vendas import Vendas
-from dataframe.stats import Stats
 from datetime import datetime
-import streamlit as st
+from math import ceil
+
 import pandas as pd
+import streamlit as st
+
+from dataframe.stats import Stats
+from dataframe.vendas import Vendas
+from utils.utils import (
+    STATUS,
+    UFS,
+    colorir_adabas,
+    colorir_equipes,
+    colorir_null_values,
+    colorir_tipo_venda,
+    default_index,
+    equipes,
+    formatar_nome,
+    get_form,
+    mask_dataframe,
+    months,
+    tipo_vendas,
+)
 
 # Configurando o layout da página
 st.set_page_config(
-    page_title = "Vendas - Freecel",
-    page_icon = "https://i.imgur.com/pidHoxz.png",
-    layout = "wide",
-    initial_sidebar_state = "expanded",
-    menu_items = {
-        'About': "https://github.com/shadyrajab/freecel-dashboard"
-    }
+    page_title="Vendas - Freecel",
+    page_icon="https://i.imgur.com/pidHoxz.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={"About": "https://github.com/shadyrajab/freecel-dashboard"},
 )
 
-with open('styles/vendas.css', 'r') as styles:
+with open("styles/vendas.css", "r") as styles:
     css = styles.read()
-    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+
 
 def load_data():
     vendas = Vendas().data.astype(str)
@@ -38,59 +43,85 @@ def load_data():
 
     return vendas, consultores, produtos
 
+
 # Sistema de paginação do DataFrame
 def split_frame(df, rows):
-    df.reset_index(drop = True, inplace = True)
+    df.reset_index(drop=True, inplace=True)
     dataframe = [df.loc[i : i + rows - 1, :] for i in range(0, len(df), rows)]
     return dataframe
+
 
 vendas, consultores, produtos = load_data()
 
 # Painel de Filtragem dos Dados
 with st.sidebar:
-    ano = st.multiselect(label = 'Ano', options = list(vendas['Ano'].unique()))
+    ano = st.multiselect(label="Ano", options=list(vendas["Ano"].unique()))
     mes = st.multiselect(
-        label = 'Mês', 
-        options = sorted(
-            list(vendas['Mês'].unique()), 
-            key = lambda x: months.index(x)
-        )
+        label="Mês",
+        options=sorted(list(vendas["Mês"].unique()), key=lambda x: months.index(x)),
     )
-    tipo = st.multiselect(label = 'Tipo', options = list(vendas['Tipo'].unique()))
-    consultor = st.multiselect(label = 'Consultor', options = list(vendas['Consultor'].unique()))
-    plano = st.multiselect(label = 'Plano', options = list(vendas['Plano'].unique()))
-    uf = st.multiselect(label = 'UF', options = list(vendas['UF'].unique()))
-    municipio = st.multiselect(label = 'Município', options = list(vendas['Município'].unique()))
-    equipe = st.multiselect(label = 'Equipe', options = list(vendas['Equipe'].unique()))
-    adabas = st.multiselect(label = 'ADABAS', options = list(vendas['ADABAS'].unique()))
+    tipo = st.multiselect(label="Tipo", options=list(vendas["Tipo"].unique()))
+    consultor = st.multiselect(
+        label="Consultor", options=list(vendas["Consultor"].unique())
+    )
+    plano = st.multiselect(label="Plano", options=list(vendas["Plano"].unique()))
+    uf = st.multiselect(label="UF", options=list(vendas["UF"].unique()))
+    municipio = st.multiselect(
+        label="Município", options=list(vendas["Município"].unique())
+    )
+    equipe = st.multiselect(label="Equipe", options=list(vendas["Equipe"].unique()))
+    adabas = st.multiselect(label="ADABAS", options=list(vendas["ADABAS"].unique()))
     default_index = st.multiselect(
-        label = 'Selecionar colunas', 
-        options = vendas.columns.to_list(), 
-        default = default_index
+        label="Selecionar colunas",
+        options=vendas.columns.to_list(),
+        default=default_index,
     )
     # Criar uma máscara booleana para cada condição de filtro
-    vendas = mask_dataframe(vendas, ano, mes, tipo, consultor, plano, equipe, municipio, uf, adabas, default_index)
-    vendas['Volume'] = vendas['Volume'].astype(int)
-    vendas['Receita'] = vendas['Receita'].astype(float)
-    vendas['Preço'] = vendas['Preço'].astype(float)
-    vendas['Data'] = pd.to_datetime(vendas['Data']).dt.strftime('%d %b %Y')
-    vendas[['Consultor', 'Gestor']] = vendas[['Consultor', 'Gestor']].map(formatar_nome)
-    vendas['Email'] = vendas['Email'].apply(lambda email: email.lower() if email != 'Não Informado' else email)
+    vendas = mask_dataframe(
+        vendas,
+        ano,
+        mes,
+        tipo,
+        consultor,
+        plano,
+        equipe,
+        municipio,
+        uf,
+        adabas,
+        default_index,
+    )
+    vendas["Volume"] = vendas["Volume"].astype(int)
+    vendas["Receita"] = vendas["Receita"].astype(float)
+    vendas["Preço"] = vendas["Preço"].astype(float)
+    vendas["Data"] = pd.to_datetime(vendas["Data"]).dt.strftime("%d %b %Y")
+    vendas[["Consultor", "Gestor"]] = vendas[["Consultor", "Gestor"]].map(formatar_nome)
+    vendas["Email"] = vendas["Email"].apply(
+        lambda email: email.lower() if email != "Não Informado" else email
+    )
 
+
+# Barra de pesquisa
+menu_superior = st.columns((4, 1, 1))
+with menu_superior[2]:
+    cnpj = st.text_input("Pesquisar CNPJ", placeholder="CNPJ")
+    if cnpj:
+        vendas = vendas[vendas["CNPJ"] == cnpj]
+
+with menu_superior[1]:
+    n_pedido = st.text_input("Pesquisar nº Pedido", placeholder="Nº Pedido")
+    if n_pedido:
+        vendas = vendas[vendas["Número do Pedido"] == n_pedido]
 # Menu inferior para a navegação nas páginas do DataFrame
 painel_de_vendas = st.container()
 menu_inferior = st.columns((4, 1, 1))
 
 with menu_inferior[2]:
-    page_size = st.selectbox("Tamanho", options = [25, 50, 100])
+    page_size = st.selectbox("Tamanho", options=[25, 50, 100])
 
 with menu_inferior[1]:
     total_pages = ceil(len(vendas) / page_size)
     current_page = st.number_input(
-        label = "Página", 
-        min_value = 1,
-        max_value = total_pages, 
-        step = 1
+        label="Página", min_value=1, max_value=total_pages, step=1
     )
 
 
@@ -101,122 +132,193 @@ pages = split_frame(vendas, page_size)
 vendas = pages[current_page - 1]
 altura = min(len(vendas) * 50, 700)
 # Definindo o estilo do DataFrame
-vendas = (vendas.style
-    .set_properties(**{'background-color': 'white'})
-    .background_gradient(subset = ['Receita'], cmap = 'Reds')._compute()
-    .background_gradient(subset = ['Preço'], cmap = 'Greens')._compute()
-    .background_gradient(subset = ['Volume'], cmap = 'Blues')._compute()
-    .background_gradient(subset = ['ID'], cmap = 'Grays')._compute()
-    .map(colorir_tipo_venda, subset = ['Tipo'])._compute()
-    .map(colorir_equipes, subset = ['Equipe'])._compute()
-    .map(colorir_null_values, subset = ['Telefone', 'Email'])._compute()
-    .map(colorir_adabas, subset = ['ADABAS'])._compute()
-    .format({'Preço': 'R$ {:.2f}'})
+vendas = (
+    vendas.style.set_properties(**{"background-color": "white"})
+    .background_gradient(subset=["Receita"], cmap="Reds")
+    ._compute()
+    .background_gradient(subset=["Preço"], cmap="Greens")
+    ._compute()
+    .background_gradient(subset=["Volume"], cmap="Blues")
+    ._compute()
+    .background_gradient(subset=["ID"], cmap="Grays")
+    ._compute()
+    .map(colorir_tipo_venda, subset=["Tipo"])
+    ._compute()
+    .map(colorir_equipes, subset=["Equipe"])
+    ._compute()
+    .map(colorir_null_values, subset=["Telefone", "Email"])
+    ._compute()
+    .map(colorir_adabas, subset=["ADABAS"])
+    ._compute()
+    .format({"Preço": "R$ {:.2f}"})
 )
 
-painel_de_vendas.dataframe(
-    data = vendas, 
-    hide_index = True,
-    height = altura,
-    use_container_width = True,
-    column_config = {
+painel_de_vendas.data_editor(
+    data=vendas,
+    disabled=[
+        "ID",
+        "CNPJ",
+        "Plano",
+        "Preço",
+        "ADABAS",
+        "CEP",
+        "Município",
+        "Bairro",
+        "Data",
+        "Número do Pedido",
+    ],
+    hide_index=True,
+    height=altura,
+    use_container_width=True,
+    column_config={
         "Volume": st.column_config.ProgressColumn(
-            label = "Volume",
-            help = "Quantidade de Produtos Vendidos.",
-            format = "%f",
-            min_value = 0,
-            max_value = 10,
+            label="Volume",
+            help="Quantidade de Produtos Vendidos.",
+            format="%f",
+            min_value=0,
+            max_value=10,
         ),
         "Preço": st.column_config.NumberColumn(
-            label = "Preço",
-            help = "Valor do Plano Vendido",
-            format = "R$ %f",
-            min_value = 0,
-            max_value = 600
+            label="Preço",
+            help="Valor do Plano Vendido",
+            format="R$ %f",
+            min_value=0,
+            max_value=600,
         ),
         "Receita": st.column_config.ProgressColumn(
-            label = "Receita",
-            help = "Valor Total Vendido",
-            format = "R$ %f",
-            min_value = 0,
-            max_value = 1000
+            label="Receita",
+            help="Valor Total Vendido",
+            format="R$ %f",
+            min_value=0,
+            max_value=1000,
         ),
-        "data": st.column_config.DatetimeColumn(
-            label = "Data",
-            help = "Data na qual a venda foi realizada",
-            format = "D MMM YYYY",
+        "Equipe": st.column_config.SelectboxColumn(
+            label="Equipe",
+            help="A equipe que realizou a venda",
+            options=equipes,
+            required=True,
         ),
-        "equipe": st.column_config.Column(
-            label = "Equipe",
-            help = "A equipe que realizou a venda"
-        )
-    }
+        "Tipo": st.column_config.SelectboxColumn(
+            label="Tipo", help="O tipo de venda", options=tipo_vendas, required=True
+        ),
+        "Consultor": st.column_config.SelectboxColumn(
+            label="Consultor",
+            help="O consultor que fez a venda",
+            options=consultores,
+            required=True,
+        ),
+        "Status": st.column_config.SelectboxColumn(
+            label="Status",
+            help="O status da venda",
+            options=STATUS,
+            required=True,
+        ),
+        "Já Cliente?": st.column_config.SelectboxColumn(
+            label="Já Cliente?",
+            help="True para Sim e False para Não",
+            options=[True, False],
+            required=True,
+        ),
+        "UF": st.column_config.SelectboxColumn(
+            label="UF", help="A UF da venda", options=UFS, required=True
+        ),
+    },
 )
 
-with st.expander('Adicionar Venda'):
+with st.expander("Adicionar Venda"):
     # Criando abas para adicionar vendas de acordo com o tipo de cliente
-    novo, migracao = st.tabs(['Novo', 'Já cliente'])
+    novo, migracao = st.tabs(["Novo", "Já cliente"])
 
     today = datetime.today().date()
 
     with novo:
-        with st.form('adicionar_venda_novo', clear_on_submit = True):
-            cnpj, telefone, consultor, data, gestor, equipe, tipo, email, volume = get_form(consultores, today)
-            plano = st.selectbox('Qual nome do plano vendido?', options = produtos)
-            token = st.text_input('Informe seu token de acesso à API.', type = 'password', placeholder = 'TOKEN')
-            submit = st.form_submit_button('Adicionar')
-            
+        with st.form("adicionar_venda_novo", clear_on_submit=True):
+            cnpj, telefone, consultor, data, gestor, equipe, tipo, email, volume = (
+                get_form(consultores, today)
+            )
+            plano = st.selectbox("Qual nome do plano vendido?", options=produtos)
+            token = st.text_input(
+                "Informe seu token de acesso à API.",
+                type="password",
+                placeholder="TOKEN",
+            )
+            submit = st.form_submit_button("Adicionar")
+
             if submit:
                 status_code = Vendas.add_venda(
-                    cnpj = cnpj, telefone = telefone, consultor = consultor, data = data, 
-                    gestor = gestor, plano = plano, volume = volume, 
-                    equipe = equipe, tipo = tipo, email = email, token = token,
-                    ja_cliente = False
+                    cnpj=cnpj,
+                    telefone=telefone,
+                    consultor=consultor,
+                    data=data,
+                    gestor=gestor,
+                    plano=plano,
+                    volume=volume,
+                    equipe=equipe,
+                    tipo=tipo,
+                    email=email,
+                    token=token,
+                    ja_cliente=False,
                 )
 
                 if status_code == 200:
 
-                    st.success('Venda adicionada com sucesso.')
+                    st.success("Venda adicionada com sucesso.")
 
                 else:
-                    st.error('Ocorreu um erro ao adicionar esta venda.')
+                    st.error("Ocorreu um erro ao adicionar esta venda.")
 
     with migracao:
-        with st.form('adicionar_venda_migracao', clear_on_submit = True):
-            cnpj, telefone, consultor, data, gestor, equipe, tipo, email, volume = get_form(consultores, today)
-            plano = st.text_input('Qual nome do plano vendido?')
-            preco = st.number_input('Qual valor da migração?')
-            token = st.text_input('Informe seu token de acesso à API.', type = 'password', placeholder = 'TOKEN')
-            submit = st.form_submit_button('Adicionar')
-            
+        with st.form("adicionar_venda_migracao", clear_on_submit=True):
+            cnpj, telefone, consultor, data, gestor, equipe, tipo, email, volume = (
+                get_form(consultores, today)
+            )
+            plano = st.text_input("Qual nome do plano vendido?")
+            preco = st.number_input("Qual valor da migração?")
+            token = st.text_input(
+                "Informe seu token de acesso à API.",
+                type="password",
+                placeholder="TOKEN",
+            )
+            submit = st.form_submit_button("Adicionar")
+
             if submit:
                 status_code = Vendas.add_venda(
-                    cnpj = cnpj, telefone = telefone, consultor = consultor, data = data, 
-                    gestor = gestor, plano = plano, volume = volume, 
-                    equipe = equipe, tipo = tipo, email = email, token = token,
-                    ja_cliente = True, preco = preco
+                    cnpj=cnpj,
+                    telefone=telefone,
+                    consultor=consultor,
+                    data=data,
+                    gestor=gestor,
+                    plano=plano,
+                    volume=volume,
+                    equipe=equipe,
+                    tipo=tipo,
+                    email=email,
+                    token=token,
+                    ja_cliente=True,
+                    preco=preco,
                 )
 
                 if status_code == 200:
-                    st.success('Venda adicionada com sucesso.')
+                    st.success("Venda adicionada com sucesso.")
 
                 else:
-                    st.error('Ocorreu um erro ao adicionar esta venda.')
+                    st.error("Ocorreu um erro ao adicionar esta venda.")
 
-with st.expander('Remover Venda'):
-    with st.form('remover_venda', clear_on_submit = True):
+with st.expander("Remover Venda"):
+    with st.form("remover_venda", clear_on_submit=True):
         id_venda = st.text_input(
-            label = 'Qual o ID da venda que deseja remover?',
-            placeholder = 'ID'
+            label="Qual o ID da venda que deseja remover?", placeholder="ID"
         )
 
-        token = st.text_input('Informe seu token de acesso à API.', type = 'password', placeholder = 'TOKEN')
-        submit = st.form_submit_button('Remover')
+        token = st.text_input(
+            "Informe seu token de acesso à API.", type="password", placeholder="TOKEN"
+        )
+        submit = st.form_submit_button("Remover")
 
         if submit:
-            status_code = Vendas.remove_venda(id = id_venda, token = token)
+            status_code = Vendas.remove_venda(id=id_venda, token=token)
             if status_code == 200:
-                st.success('Venda removida com sucesso.')
-            
+                st.success("Venda removida com sucesso.")
+
             else:
-                st.error('Ocorreu um erro ao remover esta venda.')
+                st.error("Ocorreu um erro ao remover esta venda.")
